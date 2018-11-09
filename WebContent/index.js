2e13c2aa6016a1bcea9fb51d9b1127891e68e525
路伟饶 <xiaolulwr@outlook.com>
2018-11-09 15:21:37 +0800

    Update UI & Server Code

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,40 +1,35 @@
-//访问用户媒体设备的兼容方法
+var video = document.getElementById("video");
+var canvas = document.getElementById("canvas");
+var capture = document.getElementById("capture");
+var restartcapture = document.getElementById("restartcapture");
+var upload = document.getElementById("upload");
+var context = canvas.getContext("2d");
+
 function getUserMedia(constrains,success,error){
     if(navigator.mediaDevices.getUserMedia){
-        //最新标准API
         navigator.mediaDevices.getUserMedia(constrains).then(success).catch(error);
     } else if (navigator.webkitGetUserMedia){
-        //webkit内核浏览器
         navigator.webkitGetUserMedia(constrains).then(success).catch(error);
     } else if (navigator.mozGetUserMedia){
-        //Firefox浏览器
         navagator.mozGetUserMedia(constrains).then(success).catch(error);
     } else if (navigator.getUserMedia){
-        //旧版API
         navigator.getUserMedia(constrains).then(success).catch(error);
     }
 }
 
-var video = document.getElementById("video");
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-
-//成功的回调函数
 function success(stream){
-    //兼容webkit内核浏览器
+	video.style.display="";
+	capture.style.display="";
     var CompatibleURL = window.URL || window.webkitURL;
-    //将视频流设置为video元素的源
     video.src = CompatibleURL.createObjectURL(stream);
-    //播放视频
     video.play();
 }
 
-//异常的回调函数
 function error(error){
-    console.log("访问用户媒体设备失败：",error.name,error.message);
+    alert("访问用户媒体设备失败：",error.name,error.message);
 }
+
 if (navigator.mediaDevices.getUserMedia || navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia){
-    //调用用户媒体设备，访问摄像头
     getUserMedia({
         video:{width:640,height:360}
     },success,error);
@@ -42,8 +37,23 @@ if (navigator.mediaDevices.getUserMedia || navigator.getUserMedia || navigator.w
     alert("你的浏览器不支持访问用户媒体设备");
 }
 
-//注册拍照按钮的单击事件
-document.getElementById("capture").addEventListener("click",function(){
-    //绘制画面
+function takephoto(){
     context.drawImage(video,0,0,640,360);
-});
+    canvas.style.display="";
+    video.style.display="none";
+    capture.style.display="none";
+    restartcapture.style.display="";
+    upload.style.display="";
+}
+
+function retakephoto() {
+	canvas.style.display="none";
+    video.style.display="";
+    capture.style.display="";
+    restartcapture.style.display="none";
+    upload.style.display="none";
+}
+
+function uploadphoto() {
+	
+}
--- src/main/java/com/wrlus/facedetection/api/service/FaceDetectionServiceImpl.java
+++ src/main/java/com/wrlus/facedetection/api/service/FaceDetectionServiceImpl.java
@@ -1,17 +1,88 @@
 package com.wrlus.facedetection.api.service;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.json.JSONObject;
+
+import com.baidu.ai.aip.utils.GsonUtils;
+import com.baidu.ai.aip.utils.HttpUtil;
+
 public class FaceDetectionServiceImpl implements FaceDetectionService {
 
 	@Override
 	public String getAuthToken() {
-		// TODO Auto-generated method stub
-		return null;
+		InputStream inputStream = getClass().getResourceAsStream("/config/BaiduAPIConfig.properties");
+		Properties properties = new Properties();
+		// 获取token地址
+        String authHost = "https://aip.baidubce.com/oauth/2.0/token?";
+        try {
+        	properties.load(inputStream);
+        	String getAccessTokenUrl = authHost
+                // 1. grant_type为固定参数
+                + "grant_type=client_credentials"
+                // 2. 官网获取的 API Key
+                + "&client_id=" + properties.getProperty("APIKey")
+                // 3. 官网获取的 Secret Key
+                + "&client_secret=" + properties.getProperty("SecretKey");
+        
+            URL realUrl = new URL(getAccessTokenUrl);
+            // 打开和URL之间的连接
+            HttpURLConnection connection = (HttpURLConnection) realUrl.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.err.println(key + "--->" + map.get(key));
+            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String result = "";
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+            //返回结果示例
+            System.err.println("result:" + result);
+            JSONObject jsonObject = new JSONObject(result);
+            String access_token = jsonObject.getString("access_token");
+            return access_token;
+        } catch (Exception e) {
+            System.err.printf("获取token失败！");
+            e.printStackTrace(System.err);
+        }
+        return null;
 	}
 
 	@Override
 	public String faceDetect() {
-		// TODO Auto-generated method stub
-		return null;
+		// 请求url
+        String url = "https://aip.baidubce.com/rest/2.0/face/v3/detect";
+        try {
+            Map<String, Object> map = new HashMap<>();
+            map.put("image", "027d8308a2ec665acb1bdf63e513bcb9");
+            map.put("face_field", "faceshape,facetype");
+            map.put("image_type", "FACE_TOKEN");
+
+            String param = GsonUtils.toJson(map);
+            String accessToken = getAuthToken();
+
+            String result = HttpUtil.post(url, accessToken, "application/json", param);
+            System.out.println(result);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
 	}
 
 }
